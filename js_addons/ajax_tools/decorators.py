5d87615124bcc9c698a2d73855a8981da4cfdebc
Adam Cupiał <cupial.adam@gmail.com>
2010-10-13 01:03:43 +0200

    {FIXED} :: some

--- js_addons/ajax_tools/decorators.py
+++ js_addons/ajax_tools/decorators.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from django.http import HttpResponse
 from django.utils import simplejson as json
+from django.http import HttpResponse
 
 def render_to_json(**jsonargs):
     def outer(f):
@@ -16,3 +17,18 @@ def render_to_json(**jsonargs):
             return r
         return inner_json
     return outer
+
+def ajax_messages(fn,success_message='Success',error_message='Error'):
+    def process(request, *args, **kwargs):
+        decorated = fn(request, *args, **kwargs)
+        if decorated.status_code in [200,301,302,304,307]:
+            message = success_message
+            status_code = 200
+        else:
+            message = error_message
+            status_code = 500
+        if request.is_ajax():
+            return HttpResponse(content=message, status=status_code)
+        else:
+            return decorated
+    return process
