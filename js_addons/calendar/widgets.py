ef43642310cdaf8087fcc00a3dacb1fc4574fa9c
Adam Cupiał <cupial.adam@gmail.com>
2011-05-02 10:38:32 +0200

    [ADDED] :: calendar only widget

--- js_addons/calendar/widgets.py
+++ js_addons/calendar/widgets.py
@@ -4,19 +4,39 @@ from django import forms
 import datetime, time
 from django.utils.safestring import mark_safe
 
-calbtn = u"""<img src="%scalendar/cal.gif" alt="kalendarz" id="%s_btn" style="cursor: pointer; float:left;position:relative;top:2px;" title="Kalendarz"
+calbtn = u"""<img src="%(media_url)scalendar/cal.gif" alt="kalendarz" id="%(field_id)s_btn" style="cursor: pointer; float:left;position:relative;top:2px;" title="Kalendarz"
             onmouseover="this.style.background='#444444';" onmouseout="this.style.background=''" class="calendar-button"/>
 <script type="text/javascript">
     Calendar.setup({
-        inputField     :    "%s",
-        dateFormat       :    "%s",
-        trigger :  "%s_btn",
+        inputField     :    "%(field_id)s",
+        dateFormat       :    "%(date_format)s",
+        trigger :  "%(field_id)s_btn",
         showTime  :    false,
-        titleFormat : "%s",
-        min:%s,
-        max:%s
+        titleFormat : "%(title_format)s",
+        min:%(min_date)s,
+        max:%(max_date)s
     });
 </script>"""
+
+calbtn_always_visible = u"""<div id="%(field_id)s_container" title="Kalendarz" />
+<script type="text/javascript">
+    Calendar.setup({
+        cont     :    "%(field_id)s_container",
+        dateFormat       :    "%(date_format)s",
+        showTime  :    false,
+        titleFormat : "%(title_format)s",
+        min:%(min_date)s,
+        max:%(max_date)s,
+        onSelect:function(){
+            var date = this.selection.get();
+            date = Calendar.intToDate(date);
+            date = Calendar.printDate(date,"%(date_format)s");
+            var input = document.getElementById('%(field_id)s');
+            input.value = date;
+        }
+    });
+</script>"""
+
 
 class CalendarWidget(forms.widgets.TextInput):
     """
@@ -24,12 +44,12 @@ class CalendarWidget(forms.widgets.TextInput):
     @Date: 12/10/2010
 
     Short description
-    ----------------------------
+    ------------------
 
       Django calendar widget, requires js and css files, uses Dynarch Calendar
 
     Settings
-    ----------------------------
+    ------------------
 
       date_format (default: '%Y-%m-%d'),
       title_format (default: '%b %Y')
@@ -68,13 +88,24 @@ class CalendarWidget(forms.widgets.TextInput):
             except:
                 final_attrs['value'] = \
                                    force_unicode(value)
+
         if not final_attrs.has_key('id'):
             final_attrs['id'] = u'%s_id' % (name)
         id = final_attrs['id']
 
-        cal = calbtn % (settings.MEDIA_URL, id, id, self.date_format, id, self.title_format, self.min_date, self.max_date)
+        cal = self.get_js() % {
+                'media_url':settings.MEDIA_URL,
+                'field_id':id,
+                'date_format':self.date_format,
+                'title_format':self.title_format,
+                'min_date':self.min_date, 
+                'max_date':self.max_date
+                }
         a = u'<input%s class="calendar-input"/>%s' % (forms.util.flatatt(final_attrs), cal)
         return mark_safe(a)
+    
+    def get_js(self):
+        return calbtn
 
     def value_from_datadict(self, data, files, name):
         dtf = forms.fields.DEFAULT_DATETIME_INPUT_FORMATS
@@ -93,3 +124,10 @@ class CalendarWidget(forms.widgets.TextInput):
             except ValueError:
                 continue
         return None
+
+class CalendarOnlyWidget(CalendarWidget):
+    input_type='hidden'
+    is_hidden = True
+
+    def get_js(self):
+        return calbtn_always_visible
